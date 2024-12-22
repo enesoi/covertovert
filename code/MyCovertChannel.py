f39e5ea237238c9c778bb9f5f7b01b31ab6892d2
enesoi <enesonbasi16@gmail.com>
2024-12-22 14:22:39 +0300

    Working implementation

--- code/MyCovertChannel.py
+++ code/MyCovertChannel.py
@@ -1,3 +1,8 @@
+from scapy.all import sniff, Ether, Raw
+from scapy.layers.l2 import LLC
+import random
+import time
+
 from CovertChannelBase import CovertChannelBase
 
 class MyCovertChannel(CovertChannelBase):
@@ -10,16 +15,76 @@ class MyCovertChannel(CovertChannelBase):
         - You can edit __init__.
         """
         pass
-    def send(self, log_file_name, parameter1, parameter2):
+    def send(self, log_file_name, limit):
         """
-        - In this function, you expected to create a random message (using function/s in CovertChannelBase), and send it to the receiver container. Entire sending operations should be handled in this function.
-        - After the implementation, please rewrite this comment part to explain your code basically.
-        """
-        binary_message = self.generate_random_binary_message_with_logging(log_file_name)
+        Steps:
+        - Create a random binary message.
+        - For each bit (limit parameter splits field value range into two):
+            - If 1, ctrl <- [0,limit]
+            - If 0, ctrl <- (limit,255]
+        - Packet format: Ethernet / LLC
+        - dsap and ssap are randomly generated.
+        - dst and src are written according to how Docker generates MAC addresses (basically 02:42:[IP addr. in hexadecimal format]) 
         
-    def receive(self, parameter1, parameter2, parameter3, log_file_name):
+        Current implementation requires we wait 50ms for each bit sent.
+        current capacity: 6.4 bits per second
         """
-        - In this function, you are expected to receive and decode the transferred message. Because there are many types of covert channels, the receiver implementation depends on the chosen covert channel type, and you may not need to use the functions in CovertChannelBase.
-        - After the implementation, please rewrite this comment part to explain your code basically.
+        
+        binary_message = self.generate_random_binary_message_with_logging(log_file_name, min_length=16, max_length=16)
+        print("Our message is : {}".format(binary_message))
+        
+        eth_layer = Ether(dst="02:42:ab:12:00:03", src="02:42:ab:12:00:02", type=0xaaaa)
+        for bit in binary_message:
+            if bit == "1":
+                ctrl = random.randint(0,limit)
+
+            elif bit == "0":
+                ctrl = random.randint(limit + 1, 255)
+            
+            dsap = random.randint(0,255)
+            ssap = random.randint(0,255)
+            llc_layer = LLC(dsap=dsap, ssap=ssap, ctrl=ctrl)
+            packet = eth_layer / llc_layer            
+            super().send(packet)   
+            time.sleep(0.05)     
+                
+
+        
+    def receive(self, log_file_name, limit):
         """
-        self.log_message("", log_file_name)
+        Steps:
+        - Sniff each packet one by one.
+          (the reason for [::2]): Received packets include duplicates so we skip one of each. 
+        - For some reason, scapy won't recognize LLC layer, instead shows that layer as Raw.
+          For this reason we first convert it to LLC from Raw values.
+        - If ctrl above limit, decode as 0, else 1
+        - At each 8th packet, convert it to char and append to received_msg
+        - Do until "." is read
+        """
+        
+        received_msg = ""
+        bits = ""
+        bit_count = 0
+        leave = False
+        while True:
+            packet = sniff(iface="eth0", filter= "ether proto 0xaaaa", count=1)
+            pkt = packet[0]
+            llc = LLC(pkt[Raw].load)
+            if llc.ctrl > limit:
+                bits += "0"
+            else:
+                bits += "1"
+            bit_count += 1
+            if bit_count == 8:
+                bit_count = 0
+                ch = self.convert_eight_bits_to_character(bits) 
+                received_msg += ch
+                bits = ""    
+                # print("received message = {}".format(received_msg))
+                    
+                if ch == ".":
+                    leave = True
+                    break 
+            if leave:
+                break
+        self.log_message(received_msg, log_file_name)
