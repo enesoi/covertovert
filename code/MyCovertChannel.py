c2d440e65053adc391c2d36fc1d18d45b9b3a376
enesoi <enesonbasi16@gmail.com>
2024-12-28 12:13:53 +0300

    Parametric form

--- code/MyCovertChannel.py
+++ code/MyCovertChannel.py
@@ -15,7 +15,7 @@ class MyCovertChannel(CovertChannelBase):
         - You can edit __init__.
         """
         pass
-    def send(self, log_file_name, limit):
+    def send(self, log_file_name, limit, dest_mac, source_mac):
         """
         Steps:
         - Create a random binary message.
@@ -23,17 +23,17 @@ class MyCovertChannel(CovertChannelBase):
             - If 1, ctrl <- [0,limit]
             - If 0, ctrl <- (limit,255]
         - Packet format: Ethernet / LLC
-        - dsap and ssap are randomly generated.
-        - dst and src are written according to how Docker generates MAC addresses (basically 02:42:[IP addr. in hexadecimal format]) 
+        - dsap and ssap are randomly generated. 
+        - dest_mac and source_mac are written according to how Docker generates MAC addresses (basically 02:42:[IP addr. in hexadecimal format]) 
         
         Current implementation requires we wait 50ms for each bit sent.
         current capacity: 6.4 bits per second
         """
         
         binary_message = self.generate_random_binary_message_with_logging(log_file_name, min_length=16, max_length=16)
-        print("Our message is : {}".format(binary_message))
         
-        eth_layer = Ether(dst="02:42:ab:12:00:03", src="02:42:ab:12:00:02", type=0xaaaa)
+        eth_layer = Ether(dst=dest_mac, src=source_mac, type=0xaaaa)
+        
         for bit in binary_message:
             if bit == "1":
                 ctrl = random.randint(0,limit)
@@ -70,17 +70,20 @@ class MyCovertChannel(CovertChannelBase):
             packet = sniff(iface="eth0", filter= "ether proto 0xaaaa", count=1)
             pkt = packet[0]
             llc = LLC(pkt[Raw].load)
+            
             if llc.ctrl > limit:
                 bits += "0"
+            
             else:
                 bits += "1"
+            
             bit_count += 1
+            
             if bit_count == 8:
                 bit_count = 0
                 ch = self.convert_eight_bits_to_character(bits) 
                 received_msg += ch
                 bits = ""    
-                # print("received message = {}".format(received_msg))
                     
                 if ch == ".":
                     leave = True
